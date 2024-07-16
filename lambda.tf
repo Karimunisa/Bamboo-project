3d4a7beb4bbdf1e7c8958f6ddff46aadaf639ed1
Karimunisa <98533923+Karimunisa@users.noreply.github.com>
2024-07-16 22:53:16 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "lambda_handler.py"
+  source_file = "./lambda_handler.py"
   output_path = "lambda_handler.zip"
 }
 
