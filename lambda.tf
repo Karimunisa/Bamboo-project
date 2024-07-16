82b26188b58bca1445b09dbcd8c03e674c6dcf6a
Karimunisa <98533923+Karimunisa@users.noreply.github.com>
2024-07-17 00:43:01 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,82 +1,13 @@
-# Simple AWS Lambda Terraform Example
-# requires 'lambda_handler.py' in the same directory
-# to test: run `terraform plan`
-# to deploy: run `terraform apply`
 provider "aws" {
   region = "us-east-1"
 }
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "./lambda_function.py"
-  output_path = "./lambda_function.py"
-}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
 
-resource "aws_lambda_function" "test_lambda" {
-  filename         = data.archive_file.lambda_zip.output_path
-  function_name    = "test_lambda"
-  role             = aws_iam_role.iam_for_lambda_tf.arn
-  handler          = "lambda_function.lambda_handler"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "python3.10"
-}
-
-resource "aws_iam_role" "iam_for_lambda_tf" {
-  name = "iam_for_lambda_tf"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "sts:AssumeRole"
-        Effect   = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "iam_policy_for_lambda" {
-  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
-  path        = "/"
-  description = "AWS IAM Policy for managing aws lambda role"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "iam:CreateRole",
-          "iam:AttachRolePolicy",
-          "iam:CreatePolicy",
-          "lambda:CreateFunction",
-          "lambda:UpdateFunctionCode",
-          "lambda:InvokeFunction"
-        ]
-        Resource = "*"
-        Effect   = "Allow"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role       = aws_iam_role.iam_for_lambda_tf.name
-  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
-}
-
-output "teraform_aws_role_output" {
-  value = aws_iam_role.iam_for_lambda_tf.name
-}
-
-output "teraform_aws_role_arn_output" {
-  value = aws_iam_role.iam_for_lambda_tf.arn
-}
-
-output "teraform_logging_arn_output" {
-  value = aws_iam_policy.iam_policy_for_lambda.arn
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
